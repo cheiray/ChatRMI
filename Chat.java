9572a70006d64ac67627837f0459a14f1a5fcdab
cheiray <rayencheikh@gmail.com>
2024-02-19 23:06:35 +0100

    restricted single name usage and implemented authentification

--- Chat.java
+++ Chat.java
@@ -5,9 +5,9 @@ import java.util.List;
 
 public interface Chat extends Remote{
 
-    public List<ChatMessage> getHistory() throws RemoteException;
+    public List<ChatMessage> getHistory(Client_chat client) throws RemoteException;
 
     public void newMsg(Client_chat currentClient,String msg) throws RemoteException;
-    public void connect(Client_chat client) throws RemoteException;
+    public String connect(Client_chat client) throws RemoteException;
     public void disconnect(Client_chat client) throws RemoteException;
 }
